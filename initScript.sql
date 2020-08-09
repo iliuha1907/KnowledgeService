57c9d0e33cca2f9b590e949738436b5e2f005316
Ilya Huzei <iaguzey5@gmail.com>
2020-08-09 12:47:50 +0300

    [huzei_task10] Add init script

---
+++ initScript.sql
@@ -0,0 +1,26 @@
+insert into product(model, maker, type) values('cx500','Apple','Laptop');
+insert into product(model, maker, type) values('cx600','Apple','PC');
+insert into product(model, maker, type) values('cx700','Samsung','Printer');
+insert into product(model, maker, type) values('cx400','Apple','Laptop');
+insert into product(model, maker, type) values('cx800','HP','PC');
+insert into product(model, maker, type) values('cx900','B','Laptop');
+insert into product(model, maker, type) values('cx1000','A','PC');
+insert into product(model, maker, type) values('cx200','Apple','PC');
+insert into product(model, maker, type) values('cx100','Apple','PC');
+insert into product(model, maker, type) values('cx50','Samsung','PC');
+insert into product(model, maker, type) values('cx40','Samsung','PC');
+
+insert into laptop(code, laptop_model, speed, ram, hd, price, screen) values(1,'cx500', 560, 4000, 200, 400, 15 );
+insert into pc(code, pc_model, speed, ram, hd, cd, price) values(2,'cx600', 640, 8000, 200, '4x', 400 );
+insert into printer(code, printer_model, color, price, type) values(3,'cx700', 'y', 200, 'Laser' );
+insert into laptop(code, laptop_model, speed, ram, hd, price, screen) values(4,'cx400', 260, 6000, 256, 1100, 11 );
+insert into pc(code, pc_model, speed, ram, hd, cd, price) values(5,'cx800', 460, 4000, 200, '24x', 400 );
+insert into laptop(code, laptop_model, speed, ram, hd, price, screen) values(6,'cx900', 170, 4000, 200, 300, 12 );
+insert into pc(code, pc_model, speed, ram, hd, cd, price) values(7,'cx1000', 440, 8000, 250, '4x', 410 );
+insert into pc(code, pc_model, speed, ram, hd, cd, price) values(8,'cx200', 700, 8000, 512, '4x', 800 );
+insert into pc(code, pc_model, speed, ram, hd, cd, price) values(9,'cx100', 700, 8000, 256, '8x', 600 );
+insert into pc(code, pc_model, speed, ram, hd, cd, price) values(10,'cx50', 500, 3000, 200, '4x', 400 );
+insert into pc(code, pc_model, speed, ram, hd, cd, price) values(11,'cx40', 400, 2000, 200, '4x', 400 );
+
+
+
