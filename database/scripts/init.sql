02a0bf37ec49221ef0d3bfa056d96f7e9ef2faf1
Гузей Илья Александрович <iaguzey5@gmail.com>
2020-08-31 23:04:12 +0000

    Update init.sql

--- database/scripts/init.sql
+++ database/scripts/init.sql
@@ -2,18 +2,17 @@ USE `hoteladmin_huzei` ;
 
 START TRANSACTION;
 
-INSERT INTO Rooms(status, price, capacity, stars, is_free) VALUES('SERVED',100,3,5,0),('REPAIRED',400,4,4,1),
+INSERT INTO rooms(status, price, capacity, stars, is_free) VALUES('SERVED',100,3,5,0),('REPAIRED',400,4,4,1),
 ('SERVED',200,3,5,0); 
 
-INSERT INTO Clients(first_name, last_name) VALUES('Ivan', 'Petrov'),('Serge', 'Sidorov'); 
+INSERT INTO clients(first_name, last_name) VALUES('Ivan', 'Petrov'),('Serge', 'Sidorov'); 
 
-INSERT INTO Hotel_services(price, type) VALUES(15, 'SPA'),(10, 'SAUNA');
+INSERT INTO hotel_services(price, type) VALUES(15, 'SPA'),(10, 'SAUNA');
 
-INSERT INTO Visits(client_id, hotel_service_id, date, is_active) VALUES(1,1,'2020-4-11',1),(2,2,'2020-5-11',1);
+INSERT INTO visits(client_id, hotel_service_id, date, is_active) VALUES(1,1,'2020-4-11',1),(2,2,'2020-5-11',1);
 
-INSERT INTO Reservations(room_id, resident_id, arrival_date, departure_date, is_active)
+INSERT INTO reservations(room_id, resident_id, arrival_date, departure_date, is_active)
  VALUES(1,1,'2020-4-10','2020-4-20',1),(3,2,'2020-5-10','2020-5-20',1);
 
- 
  COMMIT;
  
